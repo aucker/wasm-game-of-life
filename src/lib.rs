3ddd8bca739701caceb91a0a1a85530f318499ea
aucker <aucker@qq.com>
2022-04-01 10:34:56 +0800

    measure each invocation of `Universe::tick` takes use `web-sys` crate and `console.time` and `console.timeEnd`

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+extern crate web_sys;
+
 mod utils;
 
-use core::fmt;
+use std::fmt;
 
 use wasm_bindgen::prelude::*;
-extern crate web_sys;
+
+use web_sys::console;
 
 // A macro to provide `println!(..)`-style syntax for `console.log` logging.
 macro_rules! log {
@@ -18,6 +21,22 @@ macro_rules! log {
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+    }
+}
 
 #[wasm_bindgen]
 #[repr(u8)]
@@ -85,6 +104,8 @@ impl Universe {
 #[wasm_bindgen]
 impl Universe {
     pub fn tick(&mut self) {
+        let _timer = Timer::new("Universe::tick");
+        
         let mut next = self.cells.clone();
         
         for row in 0..self.height {
